89b295b5ade40c7212825112608273e896f646da
hakynbayo <akinbayo.akinwande@decagon.dev>
2023-03-15 02:48:58 +0100

    fetch

--- src/components/WidgetLg3.tsx
+++ src/components/WidgetLg3.tsx
@@ -1,116 +1,66 @@
-import '../css/component/widgetLg3.css'
-import '../css/component/widgetLg.css'
-import React from 'react';
+import "../css/component/widgetLg3.css";
+import "../css/component/widgetLg.css";
+import React, { useState, useEffect } from "react";
 
+interface SecurityPrice {
+  securityName: string;
+  board: string;
+  orderType: string;
+  matchedPrice: string;
+  quantity: string;
+  date: string;
+  time: string;
+}
 
-
-function WidgetLg3() {
   
+
+
+function WidgetLg3(): JSX.Element {
+  const [data, setData] = useState<SecurityPrice[]>([]);
+
+  useEffect(() => {
+    fetch("https://comx-sand-api.afex.dev/api/securities/boards", {
+      method: "GET",
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      manager.decrypt(response) // impure decryption
+      console.log(response) // something intelligible
+    });
+  }, []);
+
+
+
   return (
     <div className="widgetLg3">
       <h3 className="widgetLgTitle3">Trade Log</h3>
       <table className="widgetLgTable3">
-        <tr className="widgetLgTr">
-          <th className="widgetLgTh">Security</th>
-          <th className="widgetLgTh">Board</th>
-          <th className="widgetLgTh">Order Type</th>
-          <th className="widgetLgTh">Matched Price</th>
-          <th className="widgetLgTh">Quantity</th>
-          <th className="widgetLgTh">Date</th>
-          <th className="widgetLgTh">Time</th>
-
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <span className="widgetLgName3">Paddy Rice (SPRL)</span>
-          </td>
-          <td className="widgetLgDate">X-Traded</td>
-          <td className="widgetLgAmount">Sell</td>
-          <td className="widgetLgDate">1795.65</td>
-          <td className="widgetLgAmount">2003</td>
-          <td className="widgetLgDate">17 Oct 2020</td>
-          <td className="widgetLgStatus">
-            07:38
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <span className="widgetLgName3">Soya Beans (SSBS)</span>
-          </td>
-          <td className="widgetLgDate">X-Traded</td>
-          <td className="widgetLgAmount">Sell</td>
-          <td className="widgetLgDate">1795.65</td>
-          <td className="widgetLgAmount">86</td>
-          <td className="widgetLgDate">17 Oct 2020</td>
-          <td className="widgetLgStatus">
-            07:38
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <span className="widgetLgName3">Maize (SMAZ)</span>
-          </td>
-          <td className="widgetLgDate">OTC</td>
-          <td className="widgetLgAmount">Buy</td>
-          <td className="widgetLgDate">1795.65</td>
-          <td className="widgetLgAmount">2003</td>
-          <td className="widgetLgDate">17 Oct 2020</td>
-          <td className="widgetLgStatus">
-            07:38
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <span className="widgetLgName3">Sorghum (SSGM)</span>
-          </td>
-          <td className="widgetLgDate">FI</td>
-          <td className="widgetLgAmount">Buy</td>
-          <td className="widgetLgDate">1795.65</td>
-          <td className="widgetLgAmount">2003</td>
-          <td className="widgetLgDate">17 Oct 2020</td>
-          <td className="widgetLgStatus">
-            07:38
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <span className="widgetLgName3">Fair Trade ETC (FETC)</span>
-          </td>
-          <td className="widgetLgDate">FI</td>
-          <td className="widgetLgAmount">Buy</td>
-          <td className="widgetLgDate">1795.65</td>
-          <td className="widgetLgAmount">2003</td>
-          <td className="widgetLgDate">17 Oct 2020</td>
-          <td className="widgetLgStatus">
-            07:38
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <span className="widgetLgName3">Soya Beans (SSBS)</span>
-          </td>
-          <td className="widgetLgDate">X-Traded</td>
-          <td className="widgetLgAmount">Sell</td>
-          <td className="widgetLgDate">1795.65</td>
-          <td className="widgetLgAmount">2003</td>
-          <td className="widgetLgDate">17 Oct 2020</td>
-          <td className="widgetLgStatus">
-            07:38
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <span className="widgetLgName3">Soya Beans (SSBS)</span>
-          </td>
-          <td className="widgetLgDate">Derivates</td>
-          <td className="widgetLgAmount">Sell</td>
-          <td className="widgetLgDate">1795.65</td>
-          <td className="widgetLgAmount">2003</td>
-          <td className="widgetLgDate">17 Oct 2020</td>
-          <td className="widgetLgStatus">
-            07:38
-          </td>
-        </tr>
+        <thead>
+          <tr className="widgetLgTr">
+            <th className="widgetLgTh">Security</th>
+            <th className="widgetLgTh">Board</th>
+            <th className="widgetLgTh">Order Type</th>
+            <th className="widgetLgTh">Matched Price</th>
+            <th className="widgetLgTh">Quantity</th>
+            <th className="widgetLgTh">Date</th>
+            <th className="widgetLgTh">Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.date + item.time} className="widgetLgTr">
+              <td className="widgetLgUser">
+                <span className="widgetLgName3">{item.securityName}</span>
+              </td>
+              <td className="widgetLgDate">{item.board}</td>
+              <td className="widgetLgAmount">{item.orderType}</td>
+              <td className="widgetLgDate">{item.matchedPrice}</td>
+              <td className="widgetLgAmount">{item.quantity}</td>
+              <td className="widgetLgDate">{item.date}</td>
+              <td className="widgetLgStatus">{item.time}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
